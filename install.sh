3249ee7b640a8f4f04a69172dfa685956fed23d5
NAZMUL HOSSAIN <112247853+NazmulHossain1623@users.noreply.github.com>
2022-08-30 00:02:09 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#code_recorded_by_nazmul_hossain
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝
+                                                      
+" | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo "Loading====> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo "Loading=======> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo " NAZMUL SAID COMMAND RUN DONE " | lolcat
+sleep 1
+clear
+sleep 4
+echo ""
+echo "
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝ " | lolcat
+
+echo " FOLLOW MY FACEBOOK ACCOUNT 🔔 " | lolcat
+xdg-open https://www.facebook.com/NazmulHossain16239
+echo ""
+sleep 10
+echo " FOLLOW OUR PAGE" | lolcat
+echo " THANKS FOR FOLLOWING " | lolcat
+termux-open-url https://www.facebook.com/profile.php?id=100043316096491
+
+echo "
+
+▁▂▄▅▆▇█  𝗛𝗘𝗔𝗥 𝗪𝗘 𝗚𝗢  █▇▆▅▄▂▁
+▁▂▄▅▆▇█  𝐈𝐍𝐒𝐓𝐀𝐋𝐋 𝐃𝐎𝐍𝐄  █▇▆▅▄▂▁
+▁▂▄▅▆▇█  𝙽𝙾𝚆 𝚃𝚈𝙿𝙴 [  python2 Black.py  ] █▇▆▅▄▂▁
+▁▂▄▅▆▇█  PRESS ENTER  █▇▆▅▄▂▁" | lolcat
